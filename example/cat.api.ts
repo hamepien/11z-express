342028c37074a3e60f72a439be0f7fdbe072ab21
hamepien <hamepien@gmail.com>
2023-03-05 15:35:55 +0700

    chore: modified

--- example/cat.api.ts
+++ example/cat.api.ts
@@ -1,14 +1,14 @@
-import { Api, Context, Ctx, Get, ValidateRequest, Validation } from '../index'
-// import { CatService } from './cat.se'
+import { Api, Get, ValidateRequest, Validation } from '../index'
+import { CatService } from './cat.se'
 import zod from 'zod'
 
 @Api('/cats')
 export class CatApi {
-    // constructor(private readonly catService: CatService) {}
+    constructor(private readonly catService: CatService) {}
 
     @Get()
     @Validation(zod.object<ValidateRequest>({ query: zod.object({ name: zod.string() }) }))
-    public cat(@Ctx() ctx: Context): string {
-        return ctx.query.name as string
+    public cat(): string {
+        return this.catService.cat()
     }
 }
