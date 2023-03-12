48d29d19e196327afbad7816af74eb7053c0df00
hamepien <hamepien@gmail.com>
2023-03-12 11:27:01 +0700

    fix: middleware params error

--- dist/src/deco/api/middleware.deco.d.ts
+++ dist/src/deco/api/middleware.deco.d.ts
@@ -1,8 +1,8 @@
-import { Middleware } from '../../utils/types';
+import { Middleware, NonSafe } from '../../utils/types';
 /**
  * A function which is called before the route handler.
  *
  * @param mids execute any code.
  * @returns
  */
-export declare function Middleware(mids: Middleware[]): Function;
+export declare function Middleware(mids: Middleware[] | NonSafe[]): Function;
